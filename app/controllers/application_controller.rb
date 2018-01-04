44c509b531ae5ad6089c3ff45c0efd913db99635
dylankb <dylan.barnard@gmail.com>
2018-01-04 11:12:55 -0800

    Display persisted data in our app
    Use SQLite to import and store CSV blog post data. Create new index-like route, connect to db and query it in the controller action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
     name = params['name'] || 'World'
     render 'application/hello_world', locals: { name: name }
   end
+
+  def list_posts
+    connection = SQLite3::Database.new 'db/development.sqlite3'
+    connection.results_as_hash = true
+
+    posts = connection.execute("SELECT * FROM posts")
+
+    render 'application/list_posts', locals: { posts: posts }
+  end
 end
