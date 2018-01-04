67a65bdd1a0d1d036c1c049b5238785a861dd880
dylankb <dylan.barnard@gmail.com>
2018-01-04 10:12:24 -0800

    Use dynamic route to auto access URL params

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/hello_world' => 'application#hello_world'
+  get '/hello/:name' => 'application#hello_world'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
